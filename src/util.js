bad313e324ffdac5c98d53dd614eb256adc7e359
Anas CHERIET <anascheriet@hotmail.com>
2020-12-17 15:11:41 +0100

    Null imagePath resizing bug fixed

--- src/util.js
+++ src/util.js
@@ -1,7 +1,7 @@
 
-
-
 export const resizeImage = (imagePath, size) => {
-    const image = imagePath.match(/media\/games/) ? imagePath.replace("/media/games/", `/media/resize/${size}/-/games/`) : imagePath.replace("/media/screenshots/", `/media/resize/${size}/-/screenshots/`);
-    return image;
+    if (imagePath !== null) {
+        const image = imagePath.match(/media\/games/) ? imagePath.replace("/media/games/", `/media/resize/${size}/-/games/`) : imagePath.replace("/media/screenshots/", `/media/resize/${size}/-/screenshots/`);
+        return image;
+    }
 }
